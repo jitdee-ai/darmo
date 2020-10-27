6862bbebb692a4ba9080a7c082380a8223353a04
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-10-27 19:07:31 +1030

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,3 @@ from collections import OrderedDict
 model = darmo.create_model("relative_nas", num_classes=1000, pretrained=True)
 model.reset_classifier(num_classes=100, dropout=0.2)
 print(model)
-
-# state_dict = torch.load("ImageNet.pth.tar", map_location='cpu')['state_dict']
-
-# new_state_dict = OrderedDict()
-# for k, v in state_dict.items():
-#     name = k[7:]
-#     new_state_dict[name] = v
-
-# model.load_state_dict(new_state_dict, strict=True)
-# torch.save(model.state_dict(), "pdarts.pt")
