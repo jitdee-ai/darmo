97e3b16a2c8bdb656b5282706c8479dc27d5188d
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-10-27 19:50:20 +1030

    Updated
    Updated

--- darmo/network.py
+++ darmo/network.py
@@ -2,9 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from darmo.models.operations import *
-from darmo.models.utils import drop_path
-
+from .operations import *
+from .utils import drop_path
 
 class Cell(nn.Module):
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Chakkrit Termritthikun',
     author_email='chakkritt60@nu.ac.th',
     packages=['darmo'],
-    url='https://github.com/jitdee-ai/darts-models',
+    url='https://github.com/jitdee-ai/darmo',
     description='darts model pre-trained',
     install_requires=['torch >= 1.0', 'torchvision'],
     python_requires='>=3.6',
