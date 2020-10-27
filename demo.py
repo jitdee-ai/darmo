3cf04b27c14828c3b6a8fc1b5aabd4bd7a18f1bd
Chakkrit Termritthikun <kongpasom@gmail.com>
2020-10-28 01:03:25 +1030

    Add demo file
    Add demo file

--- demo.py
+++ demo.py
@@ -1,6 +1,4 @@
 import darmo
-import torch
-from collections import OrderedDict
 
 model = darmo.create_model("relative_nas", num_classes=1000, pretrained=True)
 model.reset_classifier(num_classes=100, dropout=0.2)
