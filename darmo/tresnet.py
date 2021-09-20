3061e22f9f56aa612f9aecee1979dc644fd2c0d8
Chakkrit Termritthikun <kongpasom@gmail.com>
2021-09-20 19:45:34 +0700

    Fixed InPlaceABN error
    Fixed InPlaceABN error

--- darmo/tresnet.py
+++ darmo/tresnet.py
@@ -200,7 +200,7 @@ class TResNet(Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
-            elif isinstance(m, nn.BatchNorm2d) or isinstance(m, InPlaceABN):
+            elif isinstance(m, nn.BatchNorm2d) or isinstance(m, InplaceAbn):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
